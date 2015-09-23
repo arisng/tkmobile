8990da428e4dafbd49673a1889169bf608c3367a
Rick Nguyen <nda1291@outlook.com>
2015-09-23 20:13:56 +0700

    Initialize Project

---
+++ TKMobile.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TKMobile.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
